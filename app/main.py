1a26c4dd197dedede156d1a763deb3df6573e9a5
Luke Tainton <ltainton@cisco.com>
2023-05-16 09:54:42 +0100

    Add Poetry, correct import sorting

--- app/main.py
+++ app/main.py
@@ -2,25 +2,27 @@
 
 """Checks if a password has been leaked."""
 
-from typing import Union
-
 import getpass
 import hashlib
+from typing import Union
+
 import requests
 
 
 def hash_password(pw_in) -> tuple:
     """Take password as input, hash it in SHA-1, and split it for use later on"""
-    pw_hash = hashlib.sha1()
+    pw_hash: hashlib._Hash = hashlib.sha1()
     pw_hash.update(str.encode(pw_in))
-    digest = pw_hash.hexdigest()
+    digest: str = pw_hash.hexdigest()
     return (digest[:5], digest[5:])
 
 
 def get_pwned_hashes(api_param) -> list:
     """Query the API for all hashes matching the input string"""
-    returned_hashes = []
-    res = requests.get(f"https://api.pwnedpasswords.com/range/{api_param}", stream=True)
+    returned_hashes: list = []
+    res: requests.Response = requests.get(
+        f"https://api.pwnedpasswords.com/range/{api_param}", timeout=10, stream=True
+    )
     for line in res.iter_lines():
         if line:
             returned_hashes.append(line.decode().split(":"))
@@ -30,14 +32,14 @@ def get_pwned_hashes(api_param) -> list:
 def check(password: str) -> Union[tuple, bool]:
     """Run password check"""
     # Get the split hash of the password
-    pw_hash_array = hash_password(password)
+    pw_hash_array: tuple = hash_password(password)
     # Send first 5 chars to API to retrieve matching hashes
-    possible_hashes = get_pwned_hashes(pw_hash_array[0])
+    possible_hashes: list = get_pwned_hashes(pw_hash_array[0])
     # For each hash, test for a match
     for possible_hash in possible_hashes:
         if pw_hash_array[1].upper() == possible_hash[0]:
-            identified_hash = pw_hash_array[0] + pw_hash_array[1]
-            occurences = possible_hash[1]
+            identified_hash: str = pw_hash_array[0] + pw_hash_array[1]
+            occurences: int = possible_hash[1]
             return (True, identified_hash, occurences)
     return False
 
@@ -45,9 +47,9 @@ def check(password: str) -> Union[tuple, bool]:
 def main() -> None:  # pragma: no cover
     """Main function"""
     # Get password
-    password = getpass.getpass()
+    password: str = getpass.getpass()
     # Run check
-    result = check(password)
+    result: Union[tuple, bool] = check(password)
     if isinstance(result, tuple) and result[0]:
         print(f"Password found as hash {result[1]}")
         print(f"Occurrences: {result[2]}")
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 
-"""Test cases for pwned_pw.py."""
+"""Test cases for app/main.py."""
 
-from app.main import hash_password, check
+from typing import Union
 
+from app.main import check, hash_password
 
 PASSWORD = "hello"
 EXPECTED_HASH = "aaf4c61ddcc5e8a2dabede0f3b482cd9aea9434d"
 
 
-def test_hash_password():
+def test_hash_password() -> None:
     """Test case for hash_password()."""
-    result = hash_password(PASSWORD)
+    result: Union[tuple, bool] = hash_password(PASSWORD)
     assert result == (EXPECTED_HASH[:5], EXPECTED_HASH[5:])
 
 
-def test_check_common_password():
+def test_check_common_password() -> None:
     """Test case for check() with common password."""
-    result = check(PASSWORD)
+    result: Union[tuple, bool] = check(PASSWORD)
     assert result[0] is True  # Password was found
     assert result[1] == EXPECTED_HASH  # Hash is correct
 
 
-def test_check_uncommon_password():
+def test_check_uncommon_password() -> None:
     """Test case for check() with uncommon password."""
-    result = check("tjzdp6eguZ9q4puPAiXWc6NM")
+    result: Union[tuple, bool] = check("tjzdp6eguZ9q4puPAiXWc6NM")
     assert result is False  # Password was not found
